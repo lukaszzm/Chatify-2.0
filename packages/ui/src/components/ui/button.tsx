3a0c75781a9cfb6acea098e74f78d2e7bc60b995
LukaszzM <lukaszzmalucha@gmail.com>
2024-05-31 09:19:15 +0200

    feat(ui): Add design colors

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-primary bg-background hover:bg-secondary hover:text-secondary-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -22,6 +22,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        xl: "h-14 rounded-lg px-10",
         icon: "h-10 w-10",
       },
     },
