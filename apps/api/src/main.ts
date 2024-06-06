37496fcf3c956ff06307fa6c1637d597a96bf8e5
LukaszzM <lukaszzmalucha@gmail.com>
2024-06-06 23:44:40 +0200

    feat(api): add fastify adapter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from "@nestjs/core";
+import type { NestFastifyApplication } from "@nestjs/platform-fastify";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 
 import { AppModule } from "@/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true,
+    }
+  );
+  await app.listen(3000, "0.0.0.0");
 }
 
 bootstrap().catch(() => {
