6cf86040729bc715c61578a61c9dcd4386335bfd
LukaszzM <lukaszzmalucha@gmail.com>
2024-06-07 11:00:05 +0200

    feat(ui): add dialog and drawer components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,8 @@
 export { cn } from "./lib/utils";
+export { useMediaQuery } from "./hooks/use-media-query";
 
 export { Button } from "./components/ui/button";
 export { Logo } from "./components/ui/logo";
+export { Dialog } from "./components/ui/dialog";
+export { Drawer } from "./components/ui/drawer";
+export { Hybrid } from "./components/ui/hybrid";
