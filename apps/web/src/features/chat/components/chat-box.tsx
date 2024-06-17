aba13477c74567575ba484f3b3793bc9e2b4c011
LukaszzM <lukaszzmalucha@gmail.com>
2024-06-17 17:31:10 +0200

    feat(web): add chat box

---
+++ apps/web/src/features/chat/components/chat-box.tsx
@@ -0,0 +1,17 @@
+import { ChatBubble } from "@chatify/ui";
+
+export const ChatBox = () => {
+  return (
+    <div className="p-4 space-y-2">
+      <ChatBubble isMine={true} createdAt="2 hours ago">
+        That&apos;s awesome. I think our users will really appreciate the improvements.
+      </ChatBubble>
+      <ChatBubble createdAt="1 hour ago" firstName="John" lastName="Smith">
+        I agree. The new design looks great.
+      </ChatBubble>
+      <ChatBubble isMine={true} createdAt="13 minutes ago">
+        Thanks! I&apos;m glad you like it.
+      </ChatBubble>
+    </div>
+  );
+};
--- apps/web/src/features/chat/components/chat.tsx
+++ apps/web/src/features/chat/components/chat.tsx
@@ -1,9 +1,11 @@
+import { ChatBox } from "@/features/chat/components/chat-box";
 import { ChatHeader } from "@/features/chat/components/chat-header";
 
 export const Chat = () => {
   return (
     <div className="bg-background rounded-md flex-1">
       <ChatHeader firstName="John" lastName="Smith" />
+      <ChatBox />
     </div>
   );
 };
