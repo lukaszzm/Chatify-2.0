28b07622b7b950c2e8e0d305aa4f4a52d0e2c151
mlukaszz <Lukasz.Malucha@columbusglobal.com>
2024-06-18 09:24:17 +0200

    feat(ui): add new button variant

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva(
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-muted text-muted-foreground hover:text-primary",
         link: "underline-offset-4 underline text-muted-foreground hover:text-primary",
+        accent: "bg-accent text-accent-foreground hover:bg-accent/90",
       },
       size: {
         default: "h-10 px-4 py-2",
