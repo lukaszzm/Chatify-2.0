c1874e6f711201bab9cf9c1c5afa2a69bd2dd0ab
mlukaszz <Lukasz.Malucha@columbusglobal.com>
2024-06-19 11:51:51 +0200

    feat(web): add notes page

--- apps/web/src/routes/_dashboard.notes.tsx
+++ apps/web/src/routes/_dashboard.notes.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Container } from "@chatify/ui";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_dashboard/notes")({
   component: NotesPage,
 });
 
 function NotesPage() {
-  return <h1>Hello from notes!</h1>;
+  return (
+    <>
+      <Container size="sm" className="space-y-4 p-4">
+        <h1 className="text-2xl">Notes</h1>
+      </Container>
+      <Outlet />
+    </>
+  );
 }
