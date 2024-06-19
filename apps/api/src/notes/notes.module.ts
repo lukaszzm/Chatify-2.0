ba50506aa18c23c377ff5b15aa15db41a535f550
mlukaszz <Lukasz.Malucha@columbusglobal.com>
2024-06-19 13:40:27 +0200

    fix(api): add missing provider to notes module

--- apps/api/src/notes/notes.module.ts
+++ apps/api/src/notes/notes.module.ts
@@ -3,8 +3,9 @@ import { PrismaService } from "nestjs-prisma";
 
 import { NotesResolver } from "@/notes/notes.resolver";
 import { NotesService } from "@/notes/notes.service";
+import { UsersService } from "@/users/users.service";
 
 @Module({
-  providers: [NotesResolver, NotesService, PrismaService],
+  providers: [NotesResolver, NotesService, PrismaService, UsersService],
 })
 export class NotesModule {}
