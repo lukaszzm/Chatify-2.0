52084c0838ebf00b7384c849fa60998409c256b8
mlukaszz <Lukasz.Malucha@columbusglobal.com>
2024-06-19 11:44:10 +0200

    refactor(web): replace div with container

--- apps/web/src/routes/_dashboard.chat.tsx
+++ apps/web/src/routes/_dashboard.chat.tsx
@@ -1,4 +1,4 @@
-import { SearchInput } from "@chatify/ui";
+import { Container, SearchInput } from "@chatify/ui";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 import { ChatList } from "@/features/chat";
@@ -10,11 +10,11 @@ export const Route = createFileRoute("/_dashboard/chat")({
 function ChatRootPage() {
   return (
     <>
-      <div className="bg-background p-4 rounded-md space-y-4 w-full max-w-xs">
+      <Container size="sm">
         <SearchInput placeholder="Search" />
         <h1 className="text-2xl">Chats</h1>
         <ChatList />
-      </div>
+      </Container>
       <Outlet />
     </>
   );
